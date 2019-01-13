83484293358547c151fa5ced2658e65a200f86a5
Abdillahi-A <39855630+Abdillahi-A@users.noreply.github.com>
2019-01-13 16:36:37 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import re
+
+def checkStatus(url):
+    res = requests.get(url)
+    if res.status_code != 200:
+        return False
+    return True
+
+def turnToSoup(url):
+    res = requests.get(url)
+    content = res.text
+    soup = BeautifulSoup(content,'lxml')
+    return soup
+
+def getUserInput():
+    songTitle = input('Enter Song Title:\n').replace(' ', '-').replace('&','and')
+    songTitle = re.sub(r'[^a-zA-Z-\d]',"",songTitle)
+    artist = input("Enter Artist:\n").replace(' ', '-')
+    artist = re.sub(r'[^a-zA-Z-\d]',"",artist)
+
+    return artist, songTitle
+
+def checkArtistExists(artist):
+    if not checkStatus(f'https://genius.com/artists/{artist}'):
+        print("Sorry we couldn't find that artist.")
+        return False
+
+    return True
+
+def getLyrics(artist, songTitle):
+    soup = turnToSoup(f'https://genius.com/{artist}-{songTitle}-lyrics')
+    lyrics = soup.find('p').text
+    print(f'\n\n{lyrics}')
+
+def main():
+    artist, songTitle = getUserInput()
+    if not checkArtistExists(artist):
+        sys.exit()
+    getLyrics(artist, songTitle)
+    
+main()
